ae6ccaaa11bdaaac6bf7c24d0e20bc0d9c1f9f13
helinjiang <me@helinjiang.com>
2018-01-31 14:35:47 +0800

    feat: 新增 web-event-recorder，用于记录页面的事件

--- demo/demo1.js
+++ demo/demo1.js
@@ -1,10 +1,10 @@
 var Nightmare = require('nightmare');
-var getAllWebEvents = require('../lib/tools/get-all-web-events').default;
+var WebEventRecorder = require('../lib/tools/web-event-recorder').default;
 
 // 更多选项可参考 https://github.com/segmentio/nightmare#nightmareoptions
 var nightmare = Nightmare({ show: true });
 
-getAllWebEvents(nightmare)
+var recorder = new WebEventRecorder(nightmare);
 
 // 执行
 nightmare.goto('https://www.baidu.com')
@@ -20,6 +20,7 @@ nightmare.goto('https://www.baidu.com')
     .end()
     .then(function (result) {
         console.log(result);
+        console.log(recorder.toString());
     })
     .catch(function (error) {
         console.error('Search failed:', error);
