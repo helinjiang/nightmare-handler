8a16b82c2ecc54e86989c3767f4dc1fb86f27081
helinjiang <me@helinjiang.com>
2018-02-16 09:49:36 +0800

    feat: 新增 getAllResponseDetail 方法，用于获取所有的资源加载情况

--- src/tools/web-event-recorder.js
+++ src/tools/web-event-recorder.js
@@ -6,22 +6,48 @@ export default class WebEventRecorder {
         this.nightmare = nightmare;
         this.queue = [];
 
+        // 初始化
         this._init();
     }
 
+    /**
+     * 记录事件到队列中
+     * @param {String} eventName 事件名称
+     * @param {Array} args 参数列表
+     */
     add(eventName, args) {
         this.queue.push(new WebEvent(eventName, args));
+    }
+
+    /**
+     * 获得所有的资源加载细节
+     * @return {[ResponseDetail]}
+     */
+    getAllResponseDetail() {
+        let result = [];
+
+        this.queue.forEach((item) => {
+            if (item.eventName !== EVENT_NAME.DID_GET_RESPONSE_DETAILS) {
+                return;
+            }
+
+            result.push(new ResponseDetail(...item.args));
+        });
+
+        return result;
     }
 
     _init() {
         let self = this;
 
         // 注册事件
-        Object.keys(EVENT_NAME).forEach((name) => {
-            this.nightmare.on(EVENT_NAME[name], function (...args) {
-                self.add(EVENT_NAME[name], args);
+        if (this.nightmare) {
+            Object.keys(EVENT_NAME).forEach((name) => {
+                this.nightmare.on(EVENT_NAME[name], function (...args) {
+                    self.add(EVENT_NAME[name], args);
+                });
             });
-        });
+        }
 
     }
 
