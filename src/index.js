8b542237737a29f1346af7c95ced7da6d8a79044
helinjiang <me@helinjiang.com>
2018-02-18 10:11:58 +0800

    fix: 修复导出对象时的问题

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import Nightmare from 'nightmare';
 import extend from './extend';
 
+export Nightmare from 'nightmare';
+export extend from './extend';
+
 export ResponseDetail from './models/response-detail';
 export WebEvent from './models/web-event';
-
 export WebEventRecorder from './tools/web-event-recorder';
 
-module.exports = extend(Nightmare, {});
+// 经过扩展等处理之后的 Nightmare
+export const NightmarePlus = extend(Nightmare, {});
