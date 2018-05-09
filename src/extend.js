9166669079036ef8bc05aa735d4dc092e0ce12c7
linjianghe <linjianghe@tencent.com>
2018-05-09 14:49:31 +0800

    feat: 将原 device 修改为 exDevice，并且新增了 exCookies 来设置cookie

--- src/extend.js
+++ src/extend.js
@@ -1,5 +1,7 @@
 export default function extend(Nightmare, config) {
     addExtendDevice(Nightmare, config);
+    
+    addExtendCookies(Nightmare);
 
     return Nightmare;
 }
@@ -20,7 +22,7 @@ function addExtendDevice(Nightmare, config) {
         }
     };
 
-    Nightmare.prototype.device = function (name, opts) {
+    Nightmare.prototype.exDevice = function (name, opts) {
         var deviceConf = Object.assign(defaultConf, config.deviceConfig);
         var opts = Object.assign({}, opts);
         var that = this;
@@ -38,4 +40,55 @@ function addExtendDevice(Nightmare, config) {
 
         return this;
     };
+}
+
+function addExtendCookies(Nightmare) {
+    Nightmare.action('exCookies',
+        function (name, options, parent, win, renderer, done) {
+            parent.on('did-start-loading', function (url, sessionCookies) {
+                if (sessionCookies) {
+                    parent.emit('log', 'Preloading cookies');
+
+                    for (var i = 0; i < sessionCookies.length; i++) {
+                        var details = Object.assign({ url: url }, sessionCookies[i]);
+                        win.webContents.session.cookies.set(details, function (error) {
+                            if (error) done(error);
+                        });
+                    }
+                }
+                parent.emit('did-start-loading');
+            });
+            done();
+            return this;
+        },
+        function (cookies, url, done) {
+            this.child.once('did-start-loading', done);
+
+            if (typeof cookies === 'string') {
+                // 如果 cookies 为字符串，则尝试转为数组
+                function getCookieObject(cookie) {
+                    let arr = cookie.split(';');
+                    let result = [];
+
+                    for (let i = 0; i < arr.length; i++) {
+                        let cur = arr[i].trim().split('=');
+
+                        result.push({
+                            name: cur[0],
+                            value: cur[1]
+                        });
+                    }
+
+                    return result;
+                }
+
+                cookies = getCookieObject(cookies);
+            } else if (!Array.isArray(cookies)) {
+                // 如果 cookies 为对象，则尝试转为对象
+                cookies = [cookies];
+            }
+
+            this.child.emit('did-start-loading', url, cookies);
+        }
+    );
 }
