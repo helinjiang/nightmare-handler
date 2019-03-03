ebbdd731aadf56acc8a73c237cc4c894950650d0
helinjiang <me@helinjiang.com>
2019-03-03 17:09:56 +0800

    docs: 文档

---
+++ demo/extend-exDevice/pc-options.js
@@ -0,0 +1,24 @@
+const nightmareHandler = require('../../lib');
+
+const nightmare = nightmareHandler.NightmarePlus({ show: true });
+
+// 执行
+nightmare
+    .exDevice('pc', { width: 600, height: 300 })
+    .goto('http://www.baidu.com')
+    .wait('#su')
+    .evaluate(function () {
+        return {
+            title: document.title,
+            searchBtnTxt: document.querySelector('#su').value,
+            width: window.innerWidth,
+            height: window.innerHeight
+        };
+    })
+    .end()
+    .then(function (result) {
+        console.log('success: ', result);
+    })
+    .catch(function (error) {
+        console.error('error: ', error);
+    });
