31ef97cb5bc79d50888ab524b78c77c718c0e388
helinjiang <me@helinjiang.com>
2019-03-03 16:40:14 +0800

    feat: 增加 exDevice 文档说明

--- src/extend.js
+++ src/extend.js
@@ -9,14 +9,14 @@ export default function extend(Nightmare, config) {
 function addExtendDevice(Nightmare, config) {
     var defaultConf = {
         'pc': {
-            'UA': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36 NIGHTMARE',
+            'UA': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36 nightmare',
             'width': 1280,
             'height': 800
         },
 
         // iphone6 尺寸为 375*667，但是nightmare中滚动条的缘故，实际尺寸应该修改为 414*760
         'mobile': {
-            'UA': 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1 TCUITEST',
+            'UA': 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1 nightmare',
             'width': 414,
             'height': 760
         }
